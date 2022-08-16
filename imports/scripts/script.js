a85ed666d3313a4c469cf39aea9a63e23246ec6d
Bruno Seiji <88354853+bruno-seiji@users.noreply.github.com>
2022-08-15 22:00:55 -0400

    side_menu e back_button ok

---
+++ imports/scripts/script.js
@@ -0,0 +1,25 @@
+new Glide('.glide').mount()
+new Glide('.glide2').mount()
+
+var side_menu = document.querySelector('.side_menu')
+var button_start = document.querySelector('.backup_button')
+
+function show_menu() {
+  side_menu.classList.toggle('menu_active')
+}
+
+function close_menu() {
+  side_menu.classList.remove('menu_active')
+}
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY <= 64) {
+    button_start.classList.add('hidden')
+  } else {
+    button_start.classList.remove('hidden')
+  }
+})
+
+function scroll_to_start() {
+  window.scrollTo({ top: 0 })
+}
